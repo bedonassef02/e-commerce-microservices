602f47fd557e8bf357f49b656f85cfa2403f5a39
Bedo <bedonassef71@gmail.com>
2024-07-25 04:46:17 +0300

    improve wishlist

--- apps/wishlist/src/commands/handlers/add-to-wishlist.handler.ts
+++ apps/wishlist/src/commands/handlers/add-to-wishlist.handler.ts
@@ -1,48 +1,45 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { WishlistService } from '../../wishlist.service';
 import { AddToWishlistCommand } from '../impl/add-to-wishlist.command';
-import { Inject } from '@nestjs/common';
+import { HttpStatus, Inject } from '@nestjs/common';
 import { PRODUCT_SERVICE } from '@app/common/utils/constants/service.constants';
-import { ClientProxy } from '@nestjs/microservices';
-import { lastValueFrom, map, switchMap } from 'rxjs';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { lastValueFrom, map } from 'rxjs';
 import { Commands } from '@app/common/utils/types/crud.interface';
-import { throwException } from '@app/common/utils/exception/throw-excpetion';
 import { WishlistDocument } from '../../entities/wishlist.entity';
+import { throwException } from '@app/common/utils/exception/throw-excpetion';
+import { notFoundException } from '@app/common/utils/exception/not-found.exception';
+import { Product } from '../../../../product/src/entities/product.entity';
 
 @CommandHandler(AddToWishlistCommand)
-export class AddToWishlistHandler
-  implements ICommandHandler<AddToWishlistCommand>
-{
+export class AddToWishlistHandler implements ICommandHandler<AddToWishlistCommand> {
   constructor(
     private readonly wishlistService: WishlistService,
     @Inject(PRODUCT_SERVICE) private productService: ClientProxy,
-  ) {}
+  ) {
+  }
 
   async execute(command: AddToWishlistCommand) {
     return lastValueFrom(
-      this.productService
-        .send(Commands.FIND_BY_ID, command.wishlistDto.product)
-        .pipe(
-          switchMap(() => {
-            return this.wishlistService
-              .findByUserId(command.wishlistDto.user)
-              .pipe(
-                map((wishlist: WishlistDocument) => {
-                  const isExist = wishlist.products.find(
-                    (p) => p == command.wishlistDto.product,
-                  );
-                  if (isExist) {
-                    return wishlist;
-                  }
-                  return this.wishlistService.update(
-                    wishlist,
-                    command.wishlistDto.product,
-                  );
-                }),
-              );
-          }),
-          throwException,
-        ),
+      this.productService.send(Commands.FIND_BY_ID, command.wishlistDto.product).pipe(
+        map(() => {
+          return this.wishlistService.findByUserId(command.wishlistDto.user).pipe(
+            map((wishlist: WishlistDocument) => {
+              const isExist = wishlist.products.find((p)=> p == command.wishlistDto.product)
+              if(isExist){
+                throw new RpcException({
+                  status: HttpStatus.CONFLICT,
+                  message: 'Product already in wishlist',
+                })
+              }
+              wishlist.products.push(command.wishlistDto.product);
+              wishlist.save();
+              return wishlist;
+            }),
+          );
+        }),
+        throwException,
+      ),
     );
   }
 }
