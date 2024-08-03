5f95575f635dff5ab2fe428c5a9b05b4279fc08c
Bedo <bedonassef71@gmail.com>
2024-08-03 22:51:22 +0300

    improve

--- libs/common/src/utils/modules/connect-to-mysql.helper.ts
+++ libs/common/src/utils/modules/connect-to-mysql.helper.ts
@@ -1,12 +1,13 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 export function connectToMysql(name: string, entities = []) {
+  // TODO: abstract
   return TypeOrmModule.forRoot({
     type: 'mysql',
     host: 'localhost',
-    port: 3306,
+    port: 3307,
     username: 'root',
-    password: '',
+    password: 'root',
     database: name,
     entities,
     synchronize: false,
